af64f0dbd5eae45750f466dc087fec601719cd7d
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2013-12-26 15:18:26 +0100

    psql all the way

--- neurovault/settings.py
+++ neurovault/settings.py
@@ -15,13 +15,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', #'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), #'neurovault',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'neurovault',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        #'USER': 'neurovault',
-        #'PASSWORD': 'neurovault',
-        #'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        #'PORT': '',                      # Set to empty string for default.
+        'USER': 'neurovault',
+        'PASSWORD': 'neurovault',
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        #'PORT': '',        # Set to empty string for default.
     }
 }
 
