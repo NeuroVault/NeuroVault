e8c11cc2cfa011e87abd903db715e65b68f02072
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2014-01-12 14:15:04 +0100

    Don't show empty collections

--- neurovault/apps/main/views.py
+++ neurovault/apps/main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from ..statmaps.models import Collection
+from django.db.models.aggregates import Count
 
 def index_view(request):
-    recent_collections = Collection.objects.all().order_by('-add_date')[:5]
+    recent_collections = Collection.objects.annotate(num_submissions=Count('image')).filter(num_submissions__gt = 0).order_by('-add_date')[:5]
     context = {'recent_collections': recent_collections}
     return render(request, 'index.html.haml', context)
