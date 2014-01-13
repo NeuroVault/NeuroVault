ad0026986181ec33b3092702c61421544d77043f
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2014-01-13 09:19:28 +0100

    URL cleanup

--- neurovault/apps/main/urls.py
+++ neurovault/apps/main/urls.py
@@ -5,8 +5,8 @@ from django.views.generic.base import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^FAQ/', TemplateView.as_view(template_name="FAQ.html.haml"), 
-        name="FAQ"),
     url(r'^$', index_view,
         name="index"),
+    url(r'^FAQ', TemplateView.as_view(template_name="FAQ.html.haml"), 
+        name="FAQ"),
 )
