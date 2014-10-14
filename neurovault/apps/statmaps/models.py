523bea9578f0cfb08714f7080c441e5f9717ef1d
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2014-10-14 14:30:45 -0700

    refactor DOI resolver

--- neurovault/apps/statmaps/models.py
+++ neurovault/apps/statmaps/models.py
@@ -133,35 +133,6 @@ class Collection(models.Model):
     class Meta:
         app_label = 'statmaps'
 
-def getPaperProperties(doi):
-    xmlurl = 'http://doi.crossref.org/servlet/query'
-    xmlpath = xmlurl + '?pid=k.j.gorgolewski@sms.ed.ac.uk&format=unixref&id=' + urllib2.quote(doi)
-    xml_str = urllib2.urlopen(xmlpath).read()
-    doc = ElementTree.fromstring(xml_str)
-    if len(doc.getchildren()) == 0 or len(doc.findall('.//crossref/error')) > 0:
-        raise Exception("DOI %s was not found" % doi)
-    title = doc.findall('.//title')[0].text
-    authors = [author.findall('given_name')[0].text + " " + author.findall('surname')[0].text for author in doc.findall('.//contributors/person_name')]
-    if len(authors) > 1:
-        authors = ", ".join(authors[:-1]) + " and " + authors[-1]
-    else:
-        authors = authors[0]
-    url = doc.findall('.//doi_data/resource')[0].text
-    date_node = doc.findall('.//publication_date')[0]
-    if len(date_node.findall('day')) > 0:
-        publication_date = datetime.date(int(date_node.findall('year')[0].text),
-                                         int(date_node.findall('month')[0].text),
-                                         int(date_node.findall('day')[0].text))
-    elif len(date_node.findall('month')) > 0:
-        publication_date = datetime.date(int(date_node.findall('year')[0].text),
-                                         int(date_node.findall('month')[0].text),
-                                         1)
-    else:
-        publication_date = datetime.date(int(date_node.findall('year')[0].text),
-                                         1,
-                                         1)
-    return title, authors, url, publication_date
-
 
 def upload_to(instance, filename):
     return os.path.join('images',str(instance.collection.id), filename)
