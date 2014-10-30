2f6cb1e1775bc7bed8ec4bec2133ad77dc222a35
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2014-10-30 14:46:31 -0700

    fixed staticdir

--- neurovault/settings.py
+++ neurovault/settings.py
@@ -71,7 +71,11 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
 
 # List of finder classes that know how to find static files in
 # various locations.
