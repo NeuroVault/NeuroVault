66d2048fe4196f647edb45251eadab47cac1add1
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2014-11-17 16:50:02 -0500

    added a test dataset

--- neurovault/apps/statmaps/tests/test_nidm_results.py
+++ neurovault/apps/statmaps/tests/test_nidm_results.py
@@ -31,5 +31,22 @@ class NIDMResultsTestCase(TestCase):
         for stat_map in parse_nidm_results("/Users/filo/krzysztof.gorgolewski@gmail.com/NeuroVault/NIDM/fsl.nidm.zip", collection):
             print stat_map.name
             print stat_map.get_estimation_method()
+            
+    def test_parse_nidm_fsl_group(self):
+        user = User(username='test')
+        user.save()
+        collection = Collection(name='test_nidm_fsl_group', owner=user)
+        collection.save()
+        for stat_map in parse_nidm_results("/Users/filo/krzysztof.gorgolewski@gmail.com/NeuroVault/NIDM/fsl_group.nidm.zip", collection):
+            print stat_map.name
+            print stat_map.get_estimation_method()
                 
+#     def test_parse_nidm_spm_group(self):
+#         user = User(username='test')
+#         user.save()
+#         collection = Collection(name='test_nidm_spm_group', owner=user)
+#         collection.save()
+#         for stat_map in parse_nidm_results("/Users/filo/krzysztof.gorgolewski@gmail.com/NeuroVault/NIDM/spm_group.nidm.zip", collection):
+#             print stat_map.name
+#             print stat_map.get_estimation_method()
                 
