31f672ed7dfc2e09668f968f890f0369a336eb6c
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2014-11-19 10:48:31 -0500

    added query by DOI option, closes #35

--- neurovault/settings.py
+++ neurovault/settings.py
@@ -214,7 +214,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
 }
 
 # Allow cross-origin requests from approved sites
