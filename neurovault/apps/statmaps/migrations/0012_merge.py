b0395d3f5ad39e7f0ef6bc6ed703959ada230ea4
gabriel rivera <rivera@infocortex.com>
2014-12-05 07:31:05 -0800

    merged migration for all newer stuff

---
+++ neurovault/apps/statmaps/migrations/0012_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('statmaps', '0005_auto_20141203_2216'),
+        ('statmaps', '0011_nidmresults'),
+    ]
+
+    operations = [
+    ]
