167af8bfc6721cbd2fdad12b25820d77dcb0f77d
jbwexler <jbwexler@stanford.edu>
2014-12-15 15:47:16 -0800

    increased 'authors' max_length to 5000

---
+++ neurovault/apps/statmaps/migrations/0013_author_max_length.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('statmaps', '0012_merge'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='collection',
+            name='authors',
+            field=models.CharField(max_length=5000, null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
