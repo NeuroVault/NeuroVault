d951076bf86a67f2949e616db2b9260d2af4b39b
gabriel rivera <rivera@infocortex.com>
2014-12-16 04:51:45 -0800

    added subtypes for admin

--- neurovault/apps/statmaps/admin.py
+++ neurovault/apps/statmaps/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
-from neurovault.apps.statmaps.models import Collection, Image 
+from neurovault.apps.statmaps.models import Collection, Image, StatisticMap, Atlas
+from neurovault.apps.statmaps.forms import StatisticMapForm, AtlasForm
+from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModelAdmin
 
-admin.site.register(Image)
+
+class StatisticMapAdmin(PolymorphicChildModelAdmin):
+    base_model = StatisticMap
+    base_form = StatisticMapForm
+
+
+class AtlasAdmin(PolymorphicChildModelAdmin):
+    base_model = Atlas
+    base_form = AtlasForm
+
+
+class ImageAdmin(PolymorphicParentModelAdmin):
+    base_model = Image
+    child_models = (
+        (StatisticMap, StatisticMapAdmin),
+        (Atlas, AtlasAdmin),
+    )
+
+
+admin.site.register(Image, ImageAdmin)
 admin.site.register(Collection)
