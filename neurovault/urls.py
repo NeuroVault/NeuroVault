fbb9096b02da73fda80d40e28c44fe45d2e8e96a
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2014-12-30 20:10:16 -0800

    More detailed error messages

--- neurovault/urls.py
+++ neurovault/urls.py
@@ -17,6 +17,7 @@ from neurovault.apps.statmaps.views import get_image,get_collection
 import re
 import xml.etree.ElementTree as ET
 import urllib2
+from django.core.handlers.base import BaseHandler
 
 from django import template
 template.add_to_builtins('django.templatetags.future')
@@ -235,3 +236,44 @@ urlpatterns = patterns('',
                        url(r'^api-auth/', include(
                            'rest_framework.urls', namespace='rest_framework'))
                        )
+
+# https://vilimpoc.org/blog/2013/07/18/detailed-error-emails-for-django-in-production-mode/                                                                         
+                                                                            
+
+def better_uncaught_exception_emails(self, request, resolver, exc_info):
+    """                                                                                                                                               
+    Processing for any otherwise uncaught exceptions (those that will                                                                                 
+    generate HTTP 500 responses). Can be overridden by subclasses who want                                                                            
+    customised 500 handling.                                                                                                                          
+                                                                                                                                                      
+    Be *very* careful when overriding this because the error could be                                                                                 
+    caused by anything, so assuming something like the database is always                                                                             
+    available would be an error.                                                                                                                      
+    """
+    from django.conf        import settings
+    from django.core.mail   import EmailMultiAlternatives
+    from django.views.debug import ExceptionReporter
+
+    # Only send details emails in the production environment.                                                                                         
+    if settings.DEBUG == False:
+        reporter = ExceptionReporter(request, *exc_info)
+
+        # Prepare the email headers for sending.   
+        # TODO move this to settings.                                                                                                
+        from_    = u"Exception Reporter <neurovaultbot@gmail.com>"
+        to_      = [admin[0] + " " + admin[1] for admin in settings.ADMINS]
+
+        subject  = "Detailed stack trace."
+
+        message = EmailMultiAlternatives(subject, reporter.get_traceback_text(), from_, to_)
+        message.attach_alternative(reporter.get_traceback_html(), 'text/html')
+        message.send()
+
+    # Make sure to then just call the base handler.                                                                                           
+    return self.original_handle_uncaught_exception(request, resolver, exc_info)
+
+# Save the original handler.                                                                                                                          
+BaseHandler.original_handle_uncaught_exception = BaseHandler.handle_uncaught_exception
+
+# Override the original handler.                                                                                                                      
+BaseHandler.handle_uncaught_exception = better_uncaught_exception_emails
