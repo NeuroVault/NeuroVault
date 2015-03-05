1edd8e6fa64f848debde41524a1ed38922aa9105
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2015-03-05 13:41:02 -0800

    added missing migration

---
+++ neurovault/apps/statmaps/migrations/0036_auto_20150305_1846.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('statmaps', '0035_auto_20150305_0413'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='statisticmap',
+            name='ignore_file_warning',
+            field=models.BooleanField(default=False, help_text=b'Ignore the warning when the map is sparse by nature, an ROI mask, or was acquired with limited field of view.', verbose_name=b'Ignore the warning about thresholding'),
+            preserve_default=True,
+        ),
+    ]
