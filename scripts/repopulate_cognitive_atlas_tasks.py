e3cb6709b0b8c80b4af81aaacfdcc7fbb268e158
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2015-03-20 10:37:59 -0700

    Updated the cog atlas term update script

--- scripts/repopulate_cognitive_atlas_tasks.py
+++ scripts/repopulate_cognitive_atlas_tasks.py
@@ -8,11 +8,9 @@ dir = os.path.abspath(os.path.dirname(__file__))
 json_content = open(os.path.join(dir, "../neurovault/apps/statmaps/migrations/cognitiveatlas_tasks.json")).read()
 json_content = json_content.decode("utf-8").replace('\t', '').replace("&#39;", "'")
 data = json.loads(json_content)
-CognitiveAtlasTask.objects.all().delete()
-CognitiveAtlasContrast.objects.all().delete()
 for item in data:
-    task = CognitiveAtlasTask(name=item["name"], cog_atlas_id=item["id"])
+    task, _ = CognitiveAtlasTask.objects.update_or_create(cog_atlas_id=item["id"], defaults={"name":item["name"]})
     task.save()
     for contrast in item["contrasts"]:
-        contrast = CognitiveAtlasContrast(name=contrast["conname"], cog_atlas_id=contrast["conid"], task=task)
+        contrast, _ = CognitiveAtlasContrast.objects.update_or_create(cog_atlas_id=contrast["conid"], defaults={"name":contrast["conname"], "task":task})
         contrast.save()
