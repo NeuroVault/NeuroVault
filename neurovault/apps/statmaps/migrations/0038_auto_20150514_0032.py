0ea5850588247a18ca20b3dd761c7aadc364af8a
jbwexler <jbwexler23@gmail.com>
2015-05-15 10:44:30 -0700

    added migrations

---
+++ neurovault/apps/statmaps/migrations/0038_auto_20150514_0032.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('statmaps', '0037_auto_20150307_0222'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='collection',
+            name='skip_factor',
+        ),
+        migrations.AddField(
+            model_name='collection',
+            name='skip_distance',
+            field=models.FloatField(help_text=b'The size of the skipped area between slices in millimeters', null=True, verbose_name=b'Skip distance', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='collection',
+            name='length_of_trials',
+            field=models.TextField(help_text=b"Length of individual trials in seconds. If variable, enter 'variable'. ", null=True, verbose_name=b'Length of trials', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='collection',
+            name='object_image_type',
+            field=models.CharField(help_text=b"What type of image was used to determine the transformation to the atlas? (e.g. 'T1', 'T2', 'EPI')", max_length=200, null=True, verbose_name=b'Object image type', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='collection',
+            name='proportion_male_subjects',
+            field=models.FloatField(blank=True, help_text=b'The proportion (not percentage) of subjects who were male', null=True, verbose_name=b'Prop. male subjects', validators=[django.core.validators.MinValueValidator(0.0), django.core.validators.MaxValueValidator(1.0)]),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='statisticmap',
+            name='cognitive_paradigm_cogatlas',
+            field=models.ForeignKey(verbose_name=b'Cognitive Paradigm', to='statmaps.CognitiveAtlasTask', help_text=b"Task (or lack of it) performed by the subjects in the scanner described using <a href='http://www.cognitiveatlas.org/' target='_blank'>Cognitive Atlas</a> terms", null=True),
+            preserve_default=True,
+        ),
+    ]
---
+++ neurovault/apps/statmaps/migrations/0039_auto_20150514_0045.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('statmaps', '0038_auto_20150514_0032'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='collection',
+            name='length_of_trials',
+            field=models.CharField(help_text=b"Length of individual trials in seconds. If variable, enter 'variable'. ", max_length=200, null=True, verbose_name=b'Length of trials', blank=True),
+            preserve_default=True,
+        ),
+    ]
