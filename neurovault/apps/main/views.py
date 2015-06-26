71f33ff48f12ce4c9690f2d416d3f00a90dd7185
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2015-06-26 15:04:33 -0700

    counting was slowing the query down

--- neurovault/apps/main/views.py
+++ neurovault/apps/main/views.py
@@ -3,6 +3,6 @@ from ..statmaps.models import Collection
 from django.db.models.aggregates import Count
 
 def index_view(request):
-    recent_collections = Collection.objects.exclude(DOI__isnull=True).exclude(private=True).annotate(num_submissions=Count('image')).filter(num_submissions__gt = 0).order_by('-add_date')[:5]
+    recent_collections = Collection.objects.exclude(DOI__isnull=True).exclude(private=True).order_by('-add_date')[:10]
     context = {'recent_collections': recent_collections}
     return render(request, 'index.html.haml', context)
