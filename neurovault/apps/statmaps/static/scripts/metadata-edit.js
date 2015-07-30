c149e87388f7f0dfb0d6b592d9cf41433749bebf
burnash <anton.burnashev@gmail.com>
2015-07-30 04:44:59 +0300

    added modal for creating new column + column name validation

--- neurovault/apps/statmaps/static/scripts/metadata-edit.js
+++ neurovault/apps/statmaps/static/scripts/metadata-edit.js
@@ -7,17 +7,16 @@
   }
 
   function hasChoices(fieldName) {
-    return NVMetadata.datasources
-      && (NVMetadata.datasources[fieldName] !== undefined);
+    return NVMetadata.datasources && (NVMetadata.datasources[fieldName] !== undefined);
   }
 
   function cache() {
     var storage = {};
     return {
-      get: function(key) {
+      get: function (key) {
         return storage[key];
       },
-      set: function(key, value) {
+      set: function (key, value) {
         storage[key] = value;
       }
     }
@@ -46,10 +45,9 @@
   function getSource(fieldName) {
     var source = NVMetadata.datasources[fieldName];
 
-    if(isString(source)) {
+    if (isString(source)) {
       return cachedAjaxSource(source);
-    }
-    else {
+    } else {
       return NVMetadata.datasources[fieldName];
     }
   }
@@ -65,7 +63,7 @@
   }
 
   function headerNames(headers) {
-    return headers.map(function(x) {
+    return headers.map(function (x) {
       if (x.required) {
         return x.name + '*';
       }
@@ -87,8 +85,7 @@
     if (datasource) {
       if (datasource.choices) {
         return NVMetadata.datasources[datasource.choices];
-      }
-      else if (datasource.url) {
+      } else if (datasource.url) {
         return cachedAjaxSource(datasource.url);
       }
     } else {
@@ -96,8 +93,12 @@
     }
   }
 
-  function stringRequiredValidator (value, callback) {
-    if (value && /\S/.test(value)) {
+  function notEmptyOrSpaced(value) {
+    return value && /\S/.test(value)
+  }
+
+  function stringRequiredValidator(value, callback) {
+    if (notEmptyOrSpaced(value)) {
       return callback(true);
     } else {
       return callback(false);
@@ -113,7 +114,7 @@
   }
 
   function columnSettings(headers) {
-    return headers.map(function(x) {
+    return headers.map(function (x) {
       return {
         type: guessFieldType(x),
         source: getDataSource(x),
@@ -134,10 +135,95 @@
     }
   }
 
+  function columnExist(columnName) {
+    var value = columnName.trim(),
+      elems = NVMetadata.headers.filter(function(x) {
+        return x.name === value;
+      });
+
+    return elems.length !== 0;
+  }
+
   function serializeTable(hotInstance) {
-      return [window.NVMetadata.headers.map(function(x) {
-          return x.name
-      })].concat(hotInstance.getData());
+    return [window.NVMetadata.headers.map(function (x) {
+      return x.name
+    })].concat(hotInstance.getData());
+  }
+
+  function validateInput(value, success, failure) {
+    if (!notEmptyOrSpaced(value)) {
+      failure();
+    }
+    else if (columnExist(value)) {
+      failure("Column already exists");
+    }
+    else {
+      success();
+    }
+  }
+
+  function showError($el, msg) {
+    $el.addClass('has-error');
+    if (msg) {
+      $('.help-block', $el).text(msg);
+    }
+  }
+
+  function clearError($el) {
+    $('.help-block', $el).empty();
+    $el.removeClass('has-error');
+  }
+
+  function getNewColumnName(callback) {
+    var $modalEl = $('#columnNameModal'),
+      $submit = $('button[type="submit"]', $modalEl),
+      $columnNameEl = $('#column-name'),
+      $inputWrapper = $('.js-input-wrapper', $modalEl);
+
+    $modalEl.on('shown.bs.modal', function () {
+      $columnNameEl.focus().attr("autocomplete", "off");
+
+      $columnNameEl.keyup(function (e) {
+        validateInput($columnNameEl.val(), function () {
+          $submit.prop('disabled', false);
+          clearError($inputWrapper);
+        }, function (msg) {
+          $submit.prop('disabled', true);
+          if (msg) {
+            showError($inputWrapper, msg);
+          }
+        });
+      });
+
+      $('.js-name-form', $modalEl).submit(function () {
+        var name = $columnNameEl.val().trim();
+        callback(name);
+        $modalEl.modal('hide');
+        $columnNameEl.val('');
+        return false;
+      });
+    });
+
+    $modalEl.on('hide.bs.modal', function() {
+      $columnNameEl.val('');
+    });
+
+    $modalEl.modal();
+  }
+
+  function insertColumn(hotInstance, selection) {
+    hotInstance.deselectCell();
+    getNewColumnName(function (name) {
+      NVMetadata.headers.push({
+        name: name
+      });
+      hotInstance.updateSettings({
+        data: window.NVMetadata.data,
+        colHeaders: headerNames(window.NVMetadata.headers),
+        columns: columnSettings(NVMetadata.headers)
+      });
+      hotInstance.render();
+    });
   }
 
   $(document).ready(function () {
@@ -145,25 +231,49 @@
     var container = document.getElementById('hot'),
       hot = new Handsontable(container, {
         data: window.NVMetadata.data,
-        stretchH: 'all',
         colHeaders: headerNames(window.NVMetadata.headers),
         columns: columnSettings(window.NVMetadata.headers),
         rowHeaders: true,
-        contextMenu: true,
         height: getDefaultHeight(window.NVMetadata.data),
+        contextMenu: {
+          callback: function (key) {
+            if (key === 'insert_column_left' ||
+              key === 'insert_column_right') {
+              insertColumn(hot, hot.getSelected());
+            }
+          },
+          items: {
+            'row_above': {},
+            'row_below': {},
+            'hsep1': '---------',
+            'insert_column_left': {
+              name: 'Insert column on the left'
+            },
+            'insert_column_right': {
+              name: 'Insert column on the right'
+            },
+            'hsep2': '---------',
+            'remove_row': {},
+            'hsep3': '---------',
+            'undo': {},
+            'redo': {}
+          }
+        },
         cells: function (r, c, prop) {
           if (c === 0) {
-            return {readOnly: true};
+            return {
+              readOnly: true
+            };
           }
         }
       });
 
     $('.btn-save-metadata').click(function () {
       $.ajax({
-        type: 'POST',
-        data: JSON.stringify(serializeTable(hot)),
-        contentType: 'application/json; charset=utf-8'
-      })
+          type: 'POST',
+          data: JSON.stringify(serializeTable(hot)),
+          contentType: 'application/json; charset=utf-8'
+        })
         .done(function () {
           var collectionId = NVMetadata.getCollectionIdFromURL(
             window.location.href
@@ -177,7 +287,7 @@
     });
 
     window.onbeforeunload = function () {
-      if (window.sheetModified) {
+      if (hot.isUndoAvailable()) {
         return 'You have pending unsaved changes. ' +
           'Do you really want to discard them ?';
       }
