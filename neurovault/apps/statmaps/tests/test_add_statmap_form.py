d7c906e9f5d8877209c3b664d3420f6030cfdaf9
Ross Blair <rblair2@stanford.edu>
2015-11-04 10:22:25 -0800

    added test to add image as owner of a collection. added password to setUp user to allow client login to work as expected

--- neurovault/apps/statmaps/tests/test_add_statmap_form.py
+++ neurovault/apps/statmaps/tests/test_add_statmap_form.py
@@ -11,10 +11,10 @@ from .utils import clearDB
 class AddStatmapsTests(TestCase):
     
     def setUp(self):
-        self.user = User.objects.create_user('NeuroGuy')
+        self.user = User.objects.create_user('NeuroGuy', password="pass")
         self.user.save()
         self.client = Client()
-        self.client.login(username=self.user)
+        self.client.login(username=self.user, password="pass")
         self.coll = Collection(owner=self.user, name="Test Collection")
         self.coll.save()
         
@@ -80,10 +80,10 @@ class AddStatmapsTests(TestCase):
             self.assertEqual(StatisticMap.objects.filter(collection=self.coll.pk)[0].name, "test map")
 
     def test_add_image_non_owner(self):
-        non_owner = User.objects.create_user('non_owner', password="test")
+        non_owner = User.objects.create_user('non_owner', password="pass")
         non_owner.save()
         client = Client()
-        client.login(username=non_owner, password="test")
+        client.login(username=non_owner, password="pass")
         testpath = os.path.abspath(os.path.dirname(__file__))
         fname_img = os.path.join(testpath,'test_data/statmaps/box_0b_vs_1b.img')
         fname_hdr = os.path.join(testpath,'test_data/statmaps/box_0b_vs_1b.hdr')
@@ -100,3 +100,24 @@ class AddStatmapsTests(TestCase):
         cid = self.coll.pk
         response = client.post(reverse('add_image', kwargs={'collection_cid':cid}), post_dict)
         self.assertEqual(response.status_code, 403) 
+
+
+    def test_add_image_owner(self):
+        testpath = os.path.abspath(os.path.dirname(__file__))
+        fname_img = os.path.join(testpath,'test_data/statmaps/box_0b_vs_1b.img')
+        fname_hdr = os.path.join(testpath,'test_data/statmaps/box_0b_vs_1b.hdr')
+        post_dict = {
+                'name': "test map",
+                'cognitive_paradigm_cogatlas': 'trm_4f24126c22011',
+                'modality':'fMRI-BOLD',
+                'map_type': 'T',
+                'collection':self.coll.pk,
+                'ignore_file_warning': True,
+                'file': SimpleUploadedFile(fname_img, open(fname_img).read()),
+                'hdr_file': SimpleUploadedFile(fname_hdr, open(fname_hdr).read())
+        }
+        cid = self.coll.pk
+        response = self.client.post(reverse('add_image', kwargs={'collection_cid':cid}), post_dict)
+        self.assertEqual(response.status_code, 302)
+        image_pk = StatisticMap.objects.order_by('-pk')[0].pk
+        self.assertRedirects(response, reverse('image_details', kwargs={'pk':image_pk}))
