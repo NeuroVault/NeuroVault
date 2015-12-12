84644ff9f16c70d9182adee4df819f37dbc78c23
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2015-12-12 10:07:51 -0800

    regenerate reduced representations as well

--- scripts/generate_missing_thumbnails.py
+++ scripts/generate_missing_thumbnails.py
@@ -5,7 +5,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neurovault.settings")
 django.setup()
 
 from neurovault.apps.statmaps.models import Image
-from neurovault.apps.statmaps.tasks import generate_glassbrain_image
+from neurovault.apps.statmaps.tasks import generate_glassbrain_image,\
+    save_resampled_transformation_single
 
 for image in Image.objects.filter(collection__private=False):
-    generate_glassbrain_image(image.id)
+    print image.id
+    generate_glassbrain_image.apply_async([image.id])
+    save_resampled_transformation_single.apply_async([image.id])
