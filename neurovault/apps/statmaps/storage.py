3970a3cac5b2704aa0c8e6d7238d369458508237
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-01-08 20:04:16 -0800

    fixed get_model

--- neurovault/apps/statmaps/storage.py
+++ neurovault/apps/statmaps/storage.py
@@ -1,26 +1,27 @@
-import os
-import itertools
 import errno
-from django.core.files.storage import FileSystemStorage
-from neurovault import settings
-from django.db.models import get_model
-from fnmatch import fnmatch
+import itertools
+import os
 import tempfile
-from django.core.files.base import File
+from fnmatch import fnmatch
+
+from django.apps import apps
 from django.core.files.move import file_move_safe
+from django.core.files.storage import FileSystemStorage
+
+from neurovault import settings
+
 
 class NeuroVaultStorage(FileSystemStorage):
-    
     def __init__(self, location=None, base_url=None):
         if location is None:
             location = settings.PRIVATE_MEDIA_ROOT
         if base_url is None:
             base_url = settings.PRIVATE_MEDIA_URL
-        return super(NeuroVaultStorage, self).__init__(location, base_url)
+        super(NeuroVaultStorage, self).__init__(location, base_url)
 
     def url(self, name):
         collection_id = None
-        spath,file_name = os.path.split(name)
+        spath, file_name = os.path.split(name)
         urlsects = [v for v in spath.split('/') if v]
         for i in range(len(urlsects)):
             sect = urlsects.pop(0)
@@ -28,16 +29,16 @@ class NeuroVaultStorage(FileSystemStorage):
                 collection_id = sect
                 break
         cont_path = '/'.join(urlsects)
-        coll_model = get_model('statmaps','Collection')
+        coll_model = apps.get_model('statmaps', 'Collection')
         collection = coll_model.objects.get(id=collection_id)
         if collection.private:
             cid = collection.private_token
         else:
             cid = collection.id
-        return os.path.join(self.base_url,str(cid),cont_path,file_name)
+        return os.path.join(self.base_url, str(cid), cont_path, file_name)
+
 
 class NiftiGzStorage(NeuroVaultStorage):
-
     def get_available_name(self, name):
         """
         Returns a filename that's free on the target storage system, and
@@ -59,14 +60,15 @@ class NiftiGzStorage(NeuroVaultStorage):
             name = os.path.join(dir_name, "%s_%s%s" % (file_root, next(count), file_ext))
 
         return name
-    
+
+
 class OverwriteStorage(NeuroVaultStorage):
     def get_available_name(self, name):
         return name
-    
+
     def _save(self, name, content):
         full_path = self.path(name)
-        
+
         # Create any intermediate directories that do not exist.
         # Note that there is a race between os.path.exists and os.makedirs:
         # if os.makedirs fails with EEXIST, the directory was created
@@ -89,26 +91,27 @@ class OverwriteStorage(NeuroVaultStorage):
                     raise
         if not os.path.isdir(directory):
             raise IOError("%s exists and is not a directory." % directory)
-        
+
         tmp_file = tempfile.mktemp()
         filey = open(tmp_file, 'wb')
         filey.write(content.read())
         # make sure that all data is on disk
         filey.flush()
-        os.fsync(filey.fileno()) 
+        os.fsync(filey.fileno())
         filey.close()
         file_move_safe(tmp_file, full_path, allow_overwrite=True)
         return name
 
+
 class NIDMStorage(NiftiGzStorage):
     def __init__(self, location=None, base_url=None):
-        return super(NIDMStorage, self).__init__(location, base_url)
+        super(NIDMStorage, self).__init__(location, base_url)
 
-    def url(self,name):
+    def url(self, name):
         rpath = super(NIDMStorage, self).url(name)
-        rpath = rpath.replace(self.base_url,'/collections').split('/')
-        for ext in ['.ttl','.provn','.zip']:
-            if fnmatch(rpath[-1],'*{0}'.format(ext)):
+        rpath = rpath.replace(self.base_url, '/collections').split('/')
+        for ext in ['.ttl', '.provn', '.zip']:
+            if fnmatch(rpath[-1], '*{0}'.format(ext)):
                 rpath.pop()
                 return '/'.join(rpath) + ext
         return '/'.join(rpath)
