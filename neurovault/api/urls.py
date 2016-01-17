9f86bad03865bb704a42f4e9fd8349a8bf44d919
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-01-16 21:49:50 -0800

    added new route /api/_my_collections listing all collections of a given user

--- neurovault/api/urls.py
+++ neurovault/api/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
-
 from rest_framework import routers
 
 from .views import (AuthUserView, ImageViewSet, AtlasViewSet,
-                    CollectionViewSet, NIDMResultsViewSet)
+                    CollectionViewSet, NIDMResultsViewSet, MyCollectionsViewSet)
 
 router = routers.DefaultRouter()
 router.register(r'images', ImageViewSet)
 router.register(r'atlases', AtlasViewSet)
 router.register(r'collections', CollectionViewSet)
+router.register(r'my_collections', MyCollectionsViewSet)
 router.register(r'nidm_results', NIDMResultsViewSet)
 
 api_urls = router.urls + [url(r'^user/?$', AuthUserView.as_view(),
