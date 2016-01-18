a3485d68f3b6b07f41b1e9391ba6cd9f7eae55e3
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-01-18 11:19:10 -0800

    fixed migration to include foreign key changes

--- neurovault/apps/statmaps/migrations/0062_move_data.py
+++ neurovault/apps/statmaps/migrations/0062_move_data.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 import taggit.managers
+from django.db import connection
 from django.db import migrations, models
 
 def move_collection_data(apps, schema_editor):
@@ -9,7 +10,9 @@ def move_collection_data(apps, schema_editor):
     BaseCollectionItem = apps.get_model("statmaps", "BaseCollectionItem")
     NIDMResults = apps.get_model("statmaps", "NIDMResults")
     ContentType = apps.get_model("contenttypes", "ContentType")
-    for image in Image.objects.all():
+    count = Image.objects.count()
+    for i, image in enumerate(Image.objects.all()):
+        print "Fixing image %d (%d/%d)"%(image.id, i+1, count)
         new_basebollectionitem = BaseCollectionItem()
         new_basebollectionitem.id = image.id
         new_basebollectionitem.name = image.name
@@ -20,20 +23,29 @@ def move_collection_data(apps, schema_editor):
         new_basebollectionitem.polymorphic_ctype = image.polymorphic_ctype
         new_basebollectionitem.tags = image.tags
         new_basebollectionitem.save()
-        image.basecollectionitem_ptr = new_basebollectionitem
+        image.basecollectionitem_ptr = new_basebollectionitem.id
         image.save()
 
-    for image in NIDMResults.objects.all():
+    cursor = connection.cursor()
+    cursor.execute("""SELECT setval(pg_get_serial_sequence('"statmaps_basecollectionitem"','id'), coalesce(max("id"), 1), max("id") IS NOT null) FROM "statmaps_basecollectionitem";""")
+
+    for nidmresult in NIDMResults.objects.all():
         new_basebollectionitem = BaseCollectionItem()
-        new_basebollectionitem.name = image.name
-        new_basebollectionitem.description = image.description
-        new_basebollectionitem.add_date = image.add_date
-        new_basebollectionitem.modify_date = image.modify_date
-        new_basebollectionitem.collection = image.collection
+        new_basebollectionitem.name = nidmresult.name
+        new_basebollectionitem.description = nidmresult.description
+        new_basebollectionitem.add_date = nidmresult.add_date
+        new_basebollectionitem.modify_date = nidmresult.modify_date
+        new_basebollectionitem.collection = nidmresult.collection
         new_basebollectionitem.polymorphic_ctype = ContentType.objects.get(app_label="statmaps", model="nidmresults")
-        new_basebollectionitem.tags = image.tags
-        image.basecollectionitem_ptr = new_basebollectionitem
-        image.save()
+        new_basebollectionitem.tags = nidmresult.tags
+        new_basebollectionitem.save()
+
+        for image in nidmresult.nidmresultstatisticmap_set.all():
+            image.nidm_results_id = new_basebollectionitem.id
+            image.save()
+
+        nidmresult.basecollectionitem_ptr = new_basebollectionitem.id
+        nidmresult.save()
 
 class Migration(migrations.Migration):
 
