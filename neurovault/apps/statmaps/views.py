5d94d69a3f1dc2d5fce75582df8eae3bae7c93b8
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-01-28 20:20:15 -0800

    fixed collection stats view

--- neurovault/apps/statmaps/views.py
+++ neurovault/apps/statmaps/views.py
@@ -695,9 +695,9 @@ def stats_view(request):
     collections_by_journals = OrderedDict(sorted(collections_by_journals.items(
                                                 ), key=lambda t: t[1], reverse=True))
 
-    non_empty_collections_count = Collection.objects.annotate(num_submissions=Count('image')).filter(num_submissions__gt = 0).count()
-    public_collections_count = Collection.objects.filter(private=False).annotate(num_submissions=Count('image')).filter(num_submissions__gt = 0).count()
-    public_collections_with_DOIs_count = Collection.objects.filter(private=False).exclude(Q(DOI__isnull=True) | Q(DOI__exact='')).annotate(num_submissions=Count('image')).filter(num_submissions__gt = 0).count()
+    non_empty_collections_count = Collection.objects.annotate(num_submissions=Count('basecollectionitem')).filter(num_submissions__gt = 0).count()
+    public_collections_count = Collection.objects.filter(private=False).annotate(num_submissions=Count('basecollectionitem')).filter(num_submissions__gt = 0).count()
+    public_collections_with_DOIs_count = Collection.objects.filter(private=False).exclude(Q(DOI__isnull=True) | Q(DOI__exact='')).annotate(num_submissions=Count('basecollectionitem')).filter(num_submissions__gt = 0).count()
     context = {'collections_by_journals': collections_by_journals,
                'non_empty_collections_count': non_empty_collections_count,
                'public_collections_count': public_collections_count,
