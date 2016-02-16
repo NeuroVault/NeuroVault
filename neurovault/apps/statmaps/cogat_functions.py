51911e01f1caef4789a98a724381cdd0c6ce5c63
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-02-15 20:13:32 -0800

    remove try/catch all

--- neurovault/apps/statmaps/cogat_functions.py
+++ neurovault/apps/statmaps/cogat_functions.py
@@ -17,22 +17,22 @@ def make_node(nid,name,color,url=None):
     else:
         return {"nid":str(nid),"name":str(name),"color":color,"url":url}
 
+
 def get_task_graph(task_id, images=None):
-    '''get_task_graph will return a tree for a single cognitive atlas tasks defined in NeuroVault
+    """get_task_graph will return a tree for a single cognitive atlas tasks defined in NeuroVault
     :param task_id: the Cognitive Atlas task id
     :param get_images_with_contrasts: boolean to return images that have contrasts (default False)
-    '''
+    """
     # Get all contrasts defined for Cognitive Atlas
     task = CognitiveAtlasTask.objects.filter(cog_atlas_id=task_id)[0]
-    task_node = make_node(task.cog_atlas_id,task.name,"#63506d")
+    task_node = make_node(task.cog_atlas_id, task.name, "#63506d")
     task_contrasts = CognitiveAtlasContrast.objects.filter(task=task)
     task_concepts = []
 
     for contrast in task_contrasts:
-        try:
-            contrast_node = make_node(contrast.cog_atlas_id,contrast.name,"#d89013")
+            contrast_node = make_node(contrast.cog_atlas_id, contrast.name, "#d89013")
             contrast_concepts = get_concept(contrast_id=contrast.cog_atlas_id)
-            children = [] # concept children of a contrast
+            children = []  # concept children of a contrast
             current_names = []
 
             # Do we have images tagged with the contrast?
@@ -41,11 +41,11 @@ def get_task_graph(task_id, images=None):
 
             for concept in contrast_concepts.json:
                 if concept["name"] not in current_names:                   
-                    concept_node = make_node(concept["id"],concept["name"],"#3c7263")
+                    concept_node = make_node(concept["id"], concept["name"], "#3c7263")
 
                     # Image nodes
-                    if len(stat_maps) > 0:
-                        stat_map_nodes = [make_node(i.pk,i.name,"#337ab7","/images/%s" %(i.pk)) for i in images]
+                    if len(images) > 0:
+                        stat_map_nodes = [make_node(i.pk, i.name, "#337ab7", "/images/%s" % i.pk) for i in images]
                         concept_node["children"] = stat_map_nodes
 
                     children.append(concept_node)
@@ -54,8 +54,7 @@ def get_task_graph(task_id, images=None):
             # Only append contrast if it has children
             if len(children) > 0:
                 task_concepts.append(contrast_node)
-        except:
-            pass
+
     task_node["children"] = task_concepts    
 
     return task_node
