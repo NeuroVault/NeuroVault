2b76741f32457018535824fc49fff05f5d6d83ab
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-02-23 15:41:12 -0800

    minor

--- neurovault/apps/statmaps/utils.py
+++ neurovault/apps/statmaps/utils.py
@@ -1,8 +1,12 @@
+import cortex
 import errno
+import nibabel as nib
+import numpy as np
 import os
-import re
 import pickle
+import pytz
 import random
+import re
 import shutil
 import string
 import subprocess
@@ -11,12 +15,6 @@ import urllib2
 import zipfile
 from ast import literal_eval
 from datetime import datetime,date
-from subprocess import CalledProcessError
-
-import cortex
-import nibabel as nib
-import numpy as np
-import pytz
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core.files.base import ContentFile
@@ -25,6 +23,7 @@ from django.core.mail import EmailMultiAlternatives
 from django.db.models import Q
 from django.template.loader import render_to_string
 from lxml import etree
+from subprocess import CalledProcessError
 
 from neurovault.apps.statmaps.models import Collection, NIDMResults, StatisticMap, Comparison, NIDMResultStatisticMap, \
     BaseStatisticMap
@@ -282,10 +281,10 @@ def split_4D_to_3D(nii,with_labels=True,tmp_dir=None):
     for n, slice in enumerate(slices):
         nifti = nib.Nifti1Image(np.squeeze(slice),nii.get_header().get_best_affine())
         layer_nm = labels[n] if n < len(labels) else 'volume_%s' % n
-        outpath = os.path.join(out_dir,'%s__%s%s' % (fname,layer_nm,ext))
+        outpath = os.path.join(out_dir, '%s__%s%s' % (fname, layer_nm, ext))
         nib.save(nifti,outpath)
         if with_labels:
-            outpaths.append((layer_nm,outpath))
+            outpaths.append((layer_nm, outpath))
         else:
             outpaths.append(outpath)
     return outpaths
