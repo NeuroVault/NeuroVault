59eb008499c6ae8b6f77f279652465459427c6f3
erramuzpe <asier.erramuzpe@gmail.com>
2016-05-03 11:49:08 +0200

    Populating db, works. already populated check pending.

--- run_uwsgi.sh
+++ run_uwsgi.sh
@@ -2,4 +2,8 @@
 # make sure tha db is up
 ./wait-for-it.sh db:5432 -- python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+python manage.py download_fixtures
+python manage.py loaddata dumpdata
+python manage.py collectmedia --noinput
+rm -R /code/neurovault/apps/statmaps/fixtures
 uwsgi uwsgi.ini
