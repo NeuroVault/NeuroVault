a8713f6ac457d7706826b34e39992af00e092a4f
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-05-02 21:05:45 -0700

    further refinements

--- scripts/sdr_email_reminder.py
+++ scripts/sdr_email_reminder.py
@@ -5,11 +5,18 @@ from neurovault.apps.statmaps.models import Collection
 
 template = """<p>Dear {username},<br \>
 Twice a year data from NeuroVault are deposited in Stanford Digital
-Repository (SDR). The mission of SDR is to preserve important scientific dataset for decades. They increase chances of long-term data accessibility by mirroring datasets at multiple data centers distributed across the globe. In two weeks we will be depositing a snapshot of NeuroVault data in SDR. Only public collections with a DOI of a corresponding paper will be included in the deposition. It seems that you currently have collections that are either private or do not have a DOI of a corresponding paper:</p>
+Repository (SDR). The mission of SDR is to preserve important scientific dataset for decades. They increase chances of
+long-term data accessibility by mirroring datasets at multiple data centers distributed across the globe. In two weeks
+we will be depositing a snapshot of NeuroVault data in SDR. Only public collections with a DOI of a corresponding paper
+will be included in the deposition. It seems that you currently have {collection_noun} that are either private or do not
+have a DOI of a corresponding paper:</p>
 <ul>
 {collections}
 </ul>
-<p>If it was work in progress and since you uploaded the maps a corresponding paper was published please log in and edit the collection metadata providing the DOI. It is also a great opportunity to upload maps, atlases, and parcellations from your other papers. Depositing data in NeuroVault increases the exposure of your research and makes it easier for other researchers to build upon thus increasing your citation rates.</p>
+<p>If it was work in progress and since you uploaded the maps a corresponding paper was published please log in and edit
+the collection metadata providing the DOI. It is also a great opportunity to upload maps, atlases, and parcellations
+from your other papers. Depositing data in NeuroVault increases the exposure of your research and makes it easier for
+other researchers to build upon thus increasing your citation rates.</p>
 
 <p>Best regards,<br />
 NeuroVault Team</p>"""
@@ -21,10 +28,14 @@ for user in User.objects.all():
         if user.first_name:
             email = template.replace("{username}", user.first_name)
         else:
-            email = template.replace("{username}", "NeuroVault user")
+            email = template.replace("{username}", "NeuroVault User")
 
-        collections_text = "\n".join(["<li><a href='http://neurovault.org%s'>%s</a></li>" % (col.name,
-                                                                                             col.get_absolute_url())
+        if len(collections) == 1:
+            email = email.replace("{collection_noun}", "a collection")
+        else:
+            email = email.replace("{collection_noun}", "collections")
+
+        collections_text = "\n".join(["<li><a href='http://neurovault.org%s'>%s</a></li>" % (col.get_absolute_url(), col.name)
                                       for col in collections])
         email = email.replace("{collections}", collections_text)
         print email
