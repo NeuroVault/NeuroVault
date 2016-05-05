b0567839ca54dda00aecd3437d77e7b53f359634
erramuzpe <asier.erramuzpe@gmail.com>
2016-05-05 17:43:22 +0200

    [WIP] Issue #29

---
+++ neurovault/apps/statmaps/management/lowercasetags.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+from neurovault.apps.statmaps.models import ValueTaggedItem, Image
+
+class Command(BaseCommand):
+    args = 'none'
+    help = 'all tag names'
+
+    def handle(self, *args, **options):
+        value_tags = ValueTaggedItem.objects.all().distinct('object_id')
+        for i, value in enumerate(value_tags):
+            image = Image.objects.all().filter(id=value.object_id)
+            tags = image[0].tags.names()
+            tags = [x.lower() for x in tags]
+            tags = list(set(tags))[:]
+            image[0].tags.clear()
+            for i, tag_name in enumerate(tags):
+                image[0].tags.add(tag_name)
