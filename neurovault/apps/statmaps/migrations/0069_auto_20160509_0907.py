6ad448d35684cbdc2f285b00a1cea320399feb2e
erramuzpe <asier.erramuzpe@gmail.com>
2016-05-11 12:16:40 +0200

    Still I can not make this work...changing approach

--- neurovault/apps/statmaps/migrations/0069_auto_20160509_0907.py
+++ neurovault/apps/statmaps/migrations/0069_auto_20160509_0907.py
@@ -6,17 +6,29 @@ from django.db import migrations, models
 def tag_sensitiveness(apps, schema_editor):
     # Find not lower case and duplicated tags and lowercase or delete them
     ValueTaggedItem = apps.get_model("statmaps", "ValueTaggedItem")
-    Image = apps.get_model("statmaps", "Image")
     KeyValueTag = apps.get_model("statmaps", "KeyValueTag")
+    print ValueTaggedItem.objects.all().count()
+    # Get a dict of all key_tags and thier names
+    tag_dict = {tag.name: tag.id for tag in KeyValueTag.objects.all()}
+    print tag_dict
+    slug_dict = {tag.id: tag.slug for tag in KeyValueTag.objects.all()}
 
-    for value in ValueTaggedItem.objects.all().distinct('object_id'):
-        image = Image.objects.all().filter(id=value.object_id)
-        tags = image[0].tags.names()
-        tags = [x.lower() for x in tags]
-        tags = list(set(tags))[:]
-        image[0].tags.clear()
-        for tag_name in tags:
-            image[0].tags.add(tag_name)
+    # Find for all tagged_items if there is a lowercased case, replace or create
+    for tag in KeyValueTag.objects.all():
+        print "Tag was %s,with slug %s, with id %s" % (tag.name,tag.slug, tag.id)
+        tag.name = tag.name.lower()
+        tag.slug = slug_dict.get(tag_dict.get(tag.name))
+        if tag_dict.has_key(tag.name):
+            tag.id = tag_dict.get(tag.name)
+        else:  # if there was not a lowercase tag associated to it
+            tag_dict[tag.name] = tag.id
+        tag.save()
+        print "Tag now is %s,with slug %s, with id %s" % (tag.name, tag.slug, tag.id)
+
+    # for item in ValueTaggedItem.objects.all():
+    #     print dir(item.tag)
+    #     print "Tag is %s,with slug %s, with id %s" % (item.tag.name,item.tag.slug, item.tag.id)
+
 
     # Find not lowercase KeyTags and delete them
     for tag in KeyValueTag.objects.all():
@@ -24,6 +36,8 @@ def tag_sensitiveness(apps, schema_editor):
             print "Deleting %s" % tag.name
             tag.delete()
 
+    print ValueTaggedItem.objects.all().count()
+    print a
 
 class Migration(migrations.Migration):
 
