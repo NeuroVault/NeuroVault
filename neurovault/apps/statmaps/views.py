266869b3c00bfe9aa5d76f3f5fb4b55077b6879d
jokedurnez <joke.durnez@gmail.com>
2016-05-13 12:31:24 -0700

    remove redundancy for neurosynth and neuropower in views

--- neurovault/apps/statmaps/views.py
+++ neurovault/apps/statmaps/views.py
@@ -434,64 +434,50 @@ def view_task(request, cog_atlas_id=None):
                "task": task }
     return render(request, 'cogatlas/cognitive_atlas_task.html', context)
 
-
+def add_image_redirect(request,app,template_path,redirect_url,is_private):
+    temp_collection_name = "%s's temporary collection" % request.user.username
+    #this is a hack we need to make sure this collection can be only
+    #owned by the same user
+    try:
+        temp_collection = Collection.objects.get(name=temp_collection_name)
+    except Collection.DoesNotExist:
+        priv_token = generate_url_token()
+        temp_collection = Collection(name=temp_collection_name,
+                                     owner=request.user,
+                                     private=is_private,
+                                     private_token=priv_token)
+        temp_collection.save()
+    image = StatisticMap(collection=temp_collection)
+    redirect = redirect_url+"/?neurovault=%s-%s" % (
+        temp_collection.private_token,image.id)
+    if request.method == "POST":
+        if app == "neurosynth":
+            form = SimplifiedStatisticMapForm(request.POST, request.FILES, instance=image, user=request.user)
+        elif app == "neuropower":
+            form = NeuropowerStatisticMapForm(request.POST, request.FILES, instance=image, user=request.user)
+        if form.is_valid():
+            image = form.save()
+            return HttpResponseRedirect(redirect)
+    else:
+        if app == "neurosynth":
+            form = SimplifiedStatisticMapForm(user=request.user, instance=image)
+        elif app == "neuropower":
+            form = NeuropowerStatisticMapForm(user=request.user, instance=image)
+    contrasts = get_contrast_lookup()
+    context = {"form": form,"contrasts":json.dumps(contrasts)}
+    return render(request,template_path , context)
 
 @login_required
 def add_image_for_neurosynth(request):
-    temp_collection_name = "%s's temporary collection" % request.user.username
-    #this is a hack we need to make sure this collection can be only
-    #owned by the same user
-    try:
-        temp_collection = Collection.objects.get(name=temp_collection_name)
-    except Collection.DoesNotExist:
-        priv_token = generate_url_token()
-        temp_collection = Collection(name=temp_collection_name,
-                                     owner=request.user,
-                                     private=False,
-                                     private_token=priv_token)
-        temp_collection.save()
-    image = StatisticMap(collection=temp_collection)
-    if request.method == "POST":
-        form = SimplifiedStatisticMapForm(request.POST, request.FILES, instance=image, user=request.user)
-        if form.is_valid():
-            image = form.save()
-            return HttpResponseRedirect("http://neurosynth.org/decode/?neurovault=%s-%s" % (
-                temp_collection.private_token,image.id))
-    else:
-        form = SimplifiedStatisticMapForm(user=request.user, instance=image)
-
-    contrasts = get_contrast_lookup()
-    context = {"form": form,"contrasts":json.dumps(contrasts)}
-    return render(request, "statmaps/add_image_for_neurosynth.html", context)
+    redirect_url = "http://neurosynth.org/decode"
+    template_path = "statmaps/add_image_for_neurosynth.html"
+    return add_image_redirect(request,"neurosynth",template_path,redirect_url,False)
 
 @login_required
 def add_image_for_neuropower(request):
-    temp_collection_name = "%s's temporary collection" % request.user.username
-    #this is a hack we need to make sure this collection can be only
-    #owned by the same user
-    try:
-        temp_collection = Collection.objects.get(name=temp_collection_name)
-    except Collection.DoesNotExist:
-        priv_token = generate_url_token()
-        temp_collection = Collection(name=temp_collection_name,
-                                     owner=request.user,
-                                     private=True,
-                                     private_token=priv_token)
-        temp_collection.save()
-    image = StatisticMap(collection=temp_collection)
-    if request.method == "POST":
-        form = NeuropowerStatisticMapForm(request.POST, request.FILES, instance=image, user=request.user)
-        if form.is_valid():
-            image = form.save()
-            return HttpResponseRedirect("http://neuropowertools.org/neuropowerinput/?neurovault=%s-%s" % (
-                temp_collection.private_token,image.id))
-    else:
-        form = NeuropowerStatisticMapForm(user=request.user, instance=image)
-
-    contrasts = get_contrast_lookup()
-    context = {"form": form,"contrasts":json.dumps(contrasts)}
-    return render(request, "statmaps/add_image_for_neuropower.html", context)
-
+    redirect_url = "http://neuropowertools.org/neuropowerinput/"
+    template_path = "statmaps/add_image_for_neuropower.html"
+    return add_image_redirect(request,"neuropower",template_path,redirect_url,True)
 
 @login_required
 def add_image(request, collection_cid):
