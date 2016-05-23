647180eaee88db5680e1027afc0e277b9df388d9
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-05-22 22:57:47 -0700

    added Richardi selection list

--- scripts/preparing_AHBA_data.py
+++ scripts/preparing_AHBA_data.py
@@ -24,12 +24,21 @@ urllib.urlretrieve(
     "https://raw.githubusercontent.com/chrisfilo/alleninf/master/alleninf/data/corrected_mni_coordinates.csv",
     os.path.join(download_dir, "corrected_mni_coordinates.csv"))
 
+#Downloading gene selections list
+urllib.urlretrieve(
+    "http://science.sciencemag.org/highwire/filestream/631209/field_highwire_adjunct_files/2/Richiardi_Data_File_S2.xlsx",
+    os.path.join(download_dir, "Richiardi_Data_File_S2.xlsx"))
+
 donors = ["H0351.2001", "H0351.2002", "H0351.1009", "H0351.1012", "H0351.1015", "H0351.1016"]
 
 # Dropping probes without entrez_id
 with zipfile.ZipFile(os.path.join(download_dir, "donor1.zip")) as z:
     with z.open("Probes.csv") as f:
-        probe_info_df = pd.read_csv(f, index_col=0)
+        probe_info_df = pd.read_csv(f)
+richardi_df = pd.read_excel(os.path.join(download_dir, "Richiardi_Data_File_S2.xlsx"))
+richardi_df.rename(columns={'Entrez_id': 'entrez_id'}, inplace=True)
+probe_info_df = pd.merge(probe_info_df, richardi_df, left_on="probe_name", right_on="probe_id",
+                         suffixes=("_original", "_richardi"), how="outer")
 
 # concatenating data from all donors
 dfs = []
@@ -39,7 +48,7 @@ for i, donor_id in enumerate(donors):
             df = pd.read_csv(f, header=None, index_col=0)
 
         # Dropping probes without entrez_id
-        df.drop(probe_info_df.index[probe_info_df['entrez_id'].isnull()], inplace=True)
+        df.drop(probe_info_df['probe_id_original'][probe_info_df['entrez_id_richardi'].isnull()], inplace=True)
 
         with z.open("SampleAnnot.csv") as f:
             sample_annot_df = pd.read_csv(f, index_col=0)
@@ -50,38 +59,17 @@ for i, donor_id in enumerate(donors):
 all_dfs = pd.concat(dfs, copy=False, axis=1)
 del dfs
 
-# adding multiindex with gene grouping
-multiindex = []
-for index in all_dfs.index:
-    entrez_id = int(probe_info_df.loc[index]["entrez_id"])
-    multiindex.append((entrez_id, index,))
-
-all_dfs.index = pd.MultiIndex.from_tuples(multiindex, names=['entrez_id', 'probe_id'])
-
-# Aggregation of probes within genes. For each gene a probe with max mean expression across all donors/wells is selected.
-means = all_dfs.mean(axis=1)
-idx_maxs = means.groupby(level=0).idxmax()
-all_dfs = all_dfs.reindex(idx_maxs)
-
 # cleaning up the probe information file
-probe_info_df.drop(probe_info_df.index[probe_info_df['entrez_id'].isnull()], inplace=True)
-multiindex = []
-for index in probe_info_df.index:
-    entrez_id = int(probe_info_df.loc[index]["entrez_id"])
-    multiindex.append((entrez_id, index,))
+probe_info_df.drop(probe_info_df.index[probe_info_df['entrez_id_richardi'].isnull()], inplace=True)
+probe_info_df.to_csv(os.path.join(download_dir, 'probe_info.csv'), index_label=False)
 
-
-probe_info_df.index = pd.MultiIndex.from_tuples(multiindex, names=['entrez_id', 'probe_id'])
-probe_info_df = probe_info_df.reindex(idx_maxs)
-probe_info_df.index = probe_info_df.index.droplevel()
-probe_info_df.to_csv(os.path.join(download_dir, 'probe_info.csv'))
+assert(all_dfs.index[0] in list(probe_info_df.probe_id_original))
 
 # saving the reduced expression data to HDF5 file for efficient readout and querying
 with pd.HDFStore(os.path.join(download_dir, 'store.h5')) as store:
-    all_dfs.index = all_dfs.index.droplevel(1)
     for donor_id in donors:
         store.append(donor_id, all_dfs[donor_id])
 
 # Removing downloaded files
 for i, url in enumerate(urls):
-    shutil.rmtree(os.path.join(download_dir, "donor%d.zip" % (i + 1)))
+    os.remove(os.path.join(download_dir, "donor%d.zip" % (i + 1)))
