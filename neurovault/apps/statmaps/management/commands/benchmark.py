b6d36d51647071f78d5f9908b47da3d6f09c1dba
erramuzpe <asier.erramuzpe@gmail.com>
2016-05-27 17:04:48 +0200

    calculating how much does it take voxelwise comp

--- neurovault/apps/statmaps/management/commands/benchmark.py
+++ neurovault/apps/statmaps/management/commands/benchmark.py
@@ -1,16 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
-import os.path
-from neurovault.apps import statmaps
-import zipfile
-import subprocess
-import tempfile
 from neurovault.apps.statmaps.tasks import run_voxelwise_pearson_similarity
 
 
-#from neurovault.apps.statmaps.models import  Image
 import gc
 import timeit
-import time
 
 class Timer:
     def __init__(self, timer=None, disable_gc=False, verbose=True):
@@ -34,20 +27,24 @@ class Timer:
         if self.verbose:
             print('time taken: %f seconds' % self.interval)
 
-
-
-
 class Command(BaseCommand):
-    rgs = '<times_to_run times_to_run ...>'
+    args = '<times_to_run times_to_run ...>'
     help = 'bench'
 
     def handle(self, *args, **options):
         from neurovault.apps.statmaps.models import Image
-        Images = Image.objects.all().filter()
+        from neurovault.apps.statmaps.utils import get_images_to_compare_with, is_search_compatible
+
+        Images = Image.objects.all()
+        for image_candidate in Images:
+            if is_search_compatible(image_candidate.pk):
+                image = image_candidate
+                break
 
         print args
         if args:
             for times_to_run in args:
+                #TODO: program this with args, a loop will be enough
                 print times_to_run
 
                 for image in Images:
@@ -56,14 +53,28 @@ class Command(BaseCommand):
                         run_voxelwise_pearson_similarity(image.pk)
                     time_taken = t.interval
                     print time_taken
-
         else:
-            for image in Images:
+            imgs_pks = get_images_to_compare_with(image.pk)
+            if imgs_pks:
+                imgs_pks.append(image.pk)
+                num_imgs = len(imgs_pks)
+                # Benchmark for 1/10 of the total images
                 t = Timer()
+                print "For 1/10 of the data (", num_imgs/10," images): "
                 with t:
-                    run_voxelwise_pearson_similarity(image.pk)
-                time_taken = t.interval
+                    for pk in imgs_pks[0:num_imgs/10]:
+                        run_voxelwise_pearson_similarity(pk)
 
+                # Benchmark for 1/2 of the total images
+                t = Timer()
+                print "For 1/2 of the data (", num_imgs/2," images): "
+                with t:
+                    for pk in imgs_pks[0:num_imgs/2]:
+                        run_voxelwise_pearson_similarity(pk)
 
-
-
+                # Benchmark for all of the total images
+                t = Timer()
+                print "For all the data (", num_imgs," images): "
+                with t:
+                    for pk in imgs_pks[0:num_imgs]:
+                        run_voxelwise_pearson_similarity(pk)
