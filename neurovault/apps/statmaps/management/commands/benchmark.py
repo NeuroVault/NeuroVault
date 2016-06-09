e9ff52e8decbb2b7d532d91a835cf22cb677a7a2
erramuzpe <asier.erramuzpe@gmail.com>
2016-06-09 14:59:16 +0200

    Changed to bench "find_similar"

--- neurovault/apps/statmaps/management/commands/benchmark.py
+++ neurovault/apps/statmaps/management/commands/benchmark.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.test.client import RequestFactory
 from neurovault.apps.statmaps.tests.utils import clearDB
-from neurovault.apps.statmaps.models import Comparison, Similarity, User, Collection, Image
-from neurovault.apps.statmaps.utils import get_existing_comparisons
-from neurovault.apps.statmaps.tests.utils import  save_statmap_form
+from neurovault.apps.statmaps.models import User, Collection
+from neurovault.apps.statmaps.views import find_similar
+from neurovault.apps.statmaps.tests.utils import save_statmap_form
 
 import os
 import gc
@@ -54,23 +55,13 @@ class Command(BaseCommand):
         clearDB()
         app_path = '/code/neurovault/apps/statmaps/tests/bench'
         u1 = User.objects.create(username='neurovault3')
-        # comparisonCollection1 = Collection(name='comparisonCollection1', owner=u1,
-        #                                         DOI='10.3389/fninf.2015.00008')
-        # comparisonCollection1.save()
-        #
-        # save_statmap_form(
-        #     image_path=os.path.join(app_path, 'images/0003.nii.gz'),
-        #     collection=comparisonCollection1,
-        #     image_name="image1",
-        #     ignore_file_warning=True)
 
         num_files = len(os.listdir(os.path.join(app_path, 'images/')))
         index_table = np.zeros(num_files)
         query_table = np.zeros(num_files)
 
         for i, file in enumerate(os.listdir(os.path.join(app_path, 'images/'))):
-            print 'Adding subject ' + file
-
+            #print 'Adding subject ' + file
             randomCollection = Collection(name='random' + file, owner=u1, DOI='10.3389/fninf.2015.00008' + str(i))
             randomCollection.save()
 
@@ -82,24 +73,12 @@ class Command(BaseCommand):
                     image_name=file,
                     ignore_file_warning=True)
             index_table[i] = t.interval
-            np.save(os.path.join(app_path, 'results_index'), index_table)
+            np.save(os.path.join(app_path, 'results_index'+ str(datetime.datetime.utcnow())[:10]), index_table)
 
+            factory = RequestFactory()
+            request = factory.get('/images/' + str(i) + '/find_similar')
             t = Timer()
             with t:
-                _dummy = get_existing_comparisons(image.pk).extra(
-                    select={"abs_score": "abs(similarity_score)"}).order_by("-abs_score")[
-                         0:100]  # "-" indicates descending # TODO: change this depending on the query function
+                _dummy = find_similar(request, image.pk)
             query_table[i] = t.interval
-            np.save(os.path.join(app_path, 'results_query' + datetime.datetime.utcnow()), query_table)
-
-
-# import matplotlib.pyplot as plt
-# import numpy as np
-# a = np.load('results_query_busy.npy')
-# b = np.load('results_query_not_busy.npy')
-# plt.plot(a,"ro",b,"bs")
-# plt.xlabel('Number of Images')
-# plt.ylabel('Seconds')
-# plt.title('Query benchmark for actual implementation')
-# plt.legend(['Busy server', 'Not busy'],loc=2)
-# plt.show()
+            np.save(os.path.join(app_path, 'results_query' + str(datetime.datetime.utcnow())[:10]), query_table)
