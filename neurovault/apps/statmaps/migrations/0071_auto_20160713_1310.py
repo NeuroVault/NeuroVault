e94d9c32ab9a38085263645b847c769dd5a74b94
erramuzpe <asier.erramuzpe@gmail.com>
2016-07-19 14:26:40 +0200

    remove old reduced representation

--- neurovault/apps/statmaps/migrations/0071_auto_20160713_1310.py
+++ neurovault/apps/statmaps/migrations/0071_auto_20160713_1310.py
@@ -15,6 +15,7 @@ def change_resample_dim(apps, schema_editor):
         try:
             os.path.exists(str(image.reduced_representation.file))
             image.reduced_representation = save_resampled_transformation_single(image.pk,  resample_dim=[16, 16, 16])
+            os.remove(str(image.reduced_representation.file))
         except ValueError:
             print "This image needs no resampling due to not previous resampled transformation"
 
