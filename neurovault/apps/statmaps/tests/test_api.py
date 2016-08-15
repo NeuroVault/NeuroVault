04cae041e2902bbdd2bdbcb8140c9d0c70d7ba71
burnash <anton.burnashev@gmail.com>
2016-08-15 22:28:32 +0300

    add a partial update test

--- neurovault/apps/statmaps/tests/test_api.py
+++ neurovault/apps/statmaps/tests/test_api.py
@@ -650,13 +650,40 @@ class TestStatisticMapChange(BaseTestCases.TestCollectionItemChange):
             'name': "renamed %s" % uuid.uuid4(),
             'modality': 'fMRI-BOLD',
             'map_type': 'T',
-            'file': file
+            'file': file,
+            'custom_metadata_numeric_field': 42
         }
 
         response = self.client.put(self.item_url, put_dict)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         self.assertEqual(response.data['name'], put_dict['name'])
+        self.assertEqual(response.data['custom_metadata_numeric_field'], 42)
+
+    def test_statistic_map_metadata_partial_update(self):
+        self.client.force_authenticate(user=self.user)
+
+        self.item.data['custom_metadata_field_a'] = 'a text'
+        self.item.data['custom_metadata_field_b'] = 'b text'
+        self.item.save()
+
+        patch_dict = {
+            'name': "renamed %s" % uuid.uuid4(),
+            'custom_metadata_field_b': 'override b with %s' % uuid.uuid4(),
+            'custom_metadata_field_c': 42,
+        }
+
+        response = self.client.patch(self.item_url, patch_dict)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(response.data['name'], patch_dict['name'])
+
+        self.assertEqual(response.data['custom_metadata_field_a'], 'a text')
+        self.assertEqual(
+            response.data['custom_metadata_field_b'],
+            patch_dict['custom_metadata_field_b']
+        )
+        self.assertEqual(response.data['custom_metadata_field_c'], 42)
 
     def test_statistic_map_destroy(self):
         self._test_collection_item_destroy(StatisticMap)
