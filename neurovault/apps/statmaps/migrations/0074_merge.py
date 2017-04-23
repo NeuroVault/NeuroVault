6b330da99e4f0080c5fa555ecbcaf4d20945ef91
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2017-04-23 12:27:11 -0700

    migration merge

---
+++ neurovault/apps/statmaps/migrations/0074_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('statmaps', '0073_auto_20161111_0033'),
+        ('statmaps', '0071_collection_topic_set'),
+    ]
+
+    operations = [
+    ]
