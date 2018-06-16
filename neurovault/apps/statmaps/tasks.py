933bae8f67089507c203a9721b930fbe0a6e94e3
J <vesaveronica@gmail.com>
2018-06-16 15:52:07 +0800

    Minor fix.

--- neurovault/apps/statmaps/tasks.py
+++ neurovault/apps/statmaps/tasks.py
@@ -206,7 +206,7 @@ def generate_surfacce_image(image_pk):
 
         img_surf = nib.gifti.GiftiImage()
         for i in range(data_dim):
-            if data_dim > 3:
+            if data_vol.ndim > 3:
                 data_curr = data_vol[:, :, :, i]
                 data_surf = interpn([range(data_vol.shape[0]), range(data_vol.shape[1]), range(data_vol.shape[2])],
                                     data_curr, vox_coor.T, 'linear')
