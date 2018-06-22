c161885167acb12d08ed209ac9f05b22a71e6608
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2018-06-22 03:29:21 -0700

    Limit surface data conversion generation to MNI only.

--- neurovault/apps/statmaps/tasks.py
+++ neurovault/apps/statmaps/tasks.py
@@ -184,52 +184,53 @@ def generate_surfacce_image(image_pk):
     from scipy.interpolate import interpn
 
     img = Image.objects.get(pk=image_pk)
-    img_vol = nib.load(img.file.path)
-    data_vol = img_vol.get_data()
-    if data_vol.ndim > 3:
-        data_dim = data_vol.shape[3]  #number of time points
-    else:
-        data_dim = 1
-    this_path = os.path.abspath(os.path.dirname(__file__))
-
-    for hemi in ['lh', 'rh']:
-        if hemi == 'lh':
-            ras_coor = loadmat(os.path.abspath(os.path.join(this_path, "static", "anatomical",
-                                                            "lh.avgMapping_allSub_RF_ANTs_MNI2fs.mat")))['ras']
+    if img.target_template_image in ['GenericMNI', 'MNI152NLin2009cAsym']:
+        img_vol = nib.load(img.file.path)
+        data_vol = img_vol.get_data()
+        if data_vol.ndim > 3:
+            data_dim = data_vol.shape[3]  #number of time points
         else:
-            ras_coor = loadmat(os.path.abspath(os.path.join(this_path, "static", "anatomical",
-                                                            "rh.avgMapping_allSub_RF_ANTs_MNI2fs.mat")))['ras']
+            data_dim = 1
+        this_path = os.path.abspath(os.path.dirname(__file__))
 
-        vox2ras = img_vol.get_sform()
-        ras_centered = ras_coor - matlib.repmat(vox2ras[0:3, 3], ras_coor.shape[1], 1).T
-        vox_coor = numpy.dot(numpy.linalg.inv(vox2ras[0:3, 0:3]), ras_centered)  # convert to voxel coordinates
-
-        img_surf = nib.gifti.GiftiImage()
-        for i in range(data_dim):
-            if data_vol.ndim > 3:
-                data_curr = data_vol[:, :, :, i]
-                data_surf = interpn([range(data_vol.shape[0]), range(data_vol.shape[1]), range(data_vol.shape[2])],
-                                    data_curr, vox_coor.T, 'linear')
-                data_surf_gifti = nib.gifti.GiftiDataArray(data_surf, 'NIFTI_INTENT_TIME_SERIES', 'NIFTI_TYPE_FLOAT32',
-                                                           'ASCII')
+        for hemi in ['lh', 'rh']:
+            if hemi == 'lh':
+                ras_coor = loadmat(os.path.abspath(os.path.join(this_path, "static", "anatomical",
+                                                                "lh.avgMapping_allSub_RF_ANTs_MNI2fs.mat")))['ras']
             else:
-                data_curr = data_vol
-                data_surf = interpn([range(data_vol.shape[0]), range(data_vol.shape[1]), range(data_vol.shape[2])],
-                                    data_curr, vox_coor.T, 'linear')
-                data_surf_gifti = nib.gifti.GiftiDataArray(data_surf, 'NIFTI_INTENT_NONE', 'NIFTI_TYPE_FLOAT32',
-                                                           'ASCII')
-            img_surf.add_gifti_data_array(data_surf_gifti)
+                ras_coor = loadmat(os.path.abspath(os.path.join(this_path, "static", "anatomical",
+                                                                "rh.avgMapping_allSub_RF_ANTs_MNI2fs.mat")))['ras']
 
-        f = BytesIO()
-        fmap = {'image': nib.FileHolder(fileobj=f), 'header': nib.FileHolder(fileobj=f)}
-        img_surf.to_file_map(fmap)
-        f.seek(0)
-        content_file = ContentFile(f.read())
-        if hemi == 'lh':
-            img.surface_left_file.save("surface_%s_%s.gii" % (hemi, img.pk), content_file)
-        else:
-            img.surface_right_file.save("surface_%s_%s.gii" % (hemi, img.pk), content_file)
-    img.save()
+            vox2ras = img_vol.get_sform()
+            ras_centered = ras_coor - matlib.repmat(vox2ras[0:3, 3], ras_coor.shape[1], 1).T
+            vox_coor = numpy.dot(numpy.linalg.inv(vox2ras[0:3, 0:3]), ras_centered)  # convert to voxel coordinates
+
+            img_surf = nib.gifti.GiftiImage()
+            for i in range(data_dim):
+                if data_vol.ndim > 3:
+                    data_curr = data_vol[:, :, :, i]
+                    data_surf = interpn([range(data_vol.shape[0]), range(data_vol.shape[1]), range(data_vol.shape[2])],
+                                        data_curr, vox_coor.T, 'linear')
+                    data_surf_gifti = nib.gifti.GiftiDataArray(data_surf, 'NIFTI_INTENT_TIME_SERIES', 'NIFTI_TYPE_FLOAT32',
+                                                               'ASCII')
+                else:
+                    data_curr = data_vol
+                    data_surf = interpn([range(data_vol.shape[0]), range(data_vol.shape[1]), range(data_vol.shape[2])],
+                                        data_curr, vox_coor.T, 'linear')
+                    data_surf_gifti = nib.gifti.GiftiDataArray(data_surf, 'NIFTI_INTENT_NONE', 'NIFTI_TYPE_FLOAT32',
+                                                               'ASCII')
+                img_surf.add_gifti_data_array(data_surf_gifti)
+
+            f = BytesIO()
+            fmap = {'image': nib.FileHolder(fileobj=f), 'header': nib.FileHolder(fileobj=f)}
+            img_surf.to_file_map(fmap)
+            f.seek(0)
+            content_file = ContentFile(f.read())
+            if hemi == 'lh':
+                img.surface_left_file.save("surface_%s_%s.gii" % (hemi, img.pk), content_file)
+            else:
+                img.surface_right_file.save("surface_%s_%s.gii" % (hemi, img.pk), content_file)
+        img.save()
     print("Surface image generation done.")
 
 
