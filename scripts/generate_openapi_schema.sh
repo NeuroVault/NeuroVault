59a1951f4159bea9ad2f13a75fde8b582597d923
James Kent <jamesdkent21@gmail.com>
2022-09-26 17:13:17 -0500

    remove url from paths

--- scripts/generate_openapi_schema.sh
+++ scripts/generate_openapi_schema.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p openapi
-./manage.py generateschema --title NeuroVault --description "All ur images R belong to us" --url="neurovault.org" --file openapi/openapi-schema.yml
+./manage.py generateschema --title NeuroVault --description "All ur images R belong to us" --file openapi/openapi-schema.yml
