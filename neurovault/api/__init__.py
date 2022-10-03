72cf3c0e6c9a5e2a5c33ba2aee67283fa2586183
James Kent <jamesdkent21@gmail.com>
2022-10-03 15:04:57 -0500

    fix import

--- neurovault/api/__init__.py
+++ neurovault/api/__init__.py
@@ -2,6 +2,6 @@
 Necessary to import so that `python manage.py spectacular`
 uses the custom authentication schema.
 """
-from schemas import JankyAuth
+from .schemas import JankyAuth
 
 __all__ = ["JankyAuth"]
