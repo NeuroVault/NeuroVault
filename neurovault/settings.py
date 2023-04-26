28136ec7c467837053965d5917f2957edcbfadef
Ross Blair <rosswilsonblair@gmail.com>
2023-04-26 12:11:21 -0500

    add tokenauthentication as option for drf authentication types

--- neurovault/settings.py
+++ neurovault/settings.py
@@ -209,6 +209,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
         "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
     ),
     "DEFAULT_RENDERER_CLASSES": (
